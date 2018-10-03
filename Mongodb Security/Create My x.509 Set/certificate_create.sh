3a37e79a1c8bf319dff7f8ad740da407076d93f5
Qi Yang <nayamama@hotmail.com>
2018-10-03 17:50:09 -0400

    add subject alternative names

--- Mongodb Security/Create My x.509 Set/certificate_create.sh
+++ Mongodb Security/Create My x.509 Set/certificate_create.sh
@@ -10,10 +10,11 @@ ou_member="MyServers"
 ou_client="MyClients"
 mongodb_server_hosts=( "server1" "server2" "server3" )
 mongodb_client_hosts=( "client1" "client2" )
-#mongodb_port=27017
-#root_ca_config="/home/vagrant/shared/root-ca.cfg"
 root_ca_config="/home/vagrant/shared/certificate_sign/root-ca.cfg"
 host_name=`hostname -f`
+root_ca_key="/home/vagrant/shared/certificate_sign/RootCA/root-ca.key"
+signing_ca_key="/home/vagrant/shared/certificate_sign/SigningCA/signing-ca.key"
+
 
 # create local root CA
 openssl genrsa -out root-ca.key 2048
@@ -99,7 +100,8 @@ openssl genrsa -out signing-ca.key 2048
 
 openssl req -new -days 1460 -key signing-ca.key -out signing-ca.csr -subj "$dn_prefix/CN=$host_name"
 #openssl ca -batch -name RootCA -config $root_ca_config -extensions v3_ca -out signing-ca.crt -infiles signing-ca.csr 
-openssl ca -batch -notext -name RootCA -config $root_ca_config -keyfile /home/vagrant/shared/certificate_sign/RootCA/root-ca.key -out signing-ca.crt -infiles signing-ca.csr 
+# openssl ca -batch -notext -name RootCA -config $root_ca_config -keyfile /home/vagrant/shared/certificate_sign/RootCA/root-ca.key -out signing-ca.crt -infiles signing-ca.csr 
+openssl ca -batch -notext -name RootCA -config $root_ca_config -extensions v3_req -keyfile $root_ca_key -out signing-ca.crt -infiles signing-ca.csr 
 
 mkdir -p SigningCA/ca.db.certs
 echo "01" >> SigningCA/ca.db.serial
@@ -117,7 +119,7 @@ for host in "${mongodb_server_hosts[@]}"; do
     echo "Generating key for $host"
     openssl genrsa  -out ${host}.key 2048
     openssl req -new -days 365 -key ${host}.key -out ${host}.csr -subj "$dn_prefix/OU=$ou_member/CN=$host_name"
-    openssl ca -batch -notext -name SigningCA -config $root_ca_config -keyfile /home/vagrant/shared/certificate_sign/SigningCA/signing-ca.key -out ${host}.crt -infiles ${host}.csr
+    openssl ca -batch -notext -name SigningCA -config $root_ca_config -extensions v3_req -keyfile $signing_ca_key -out ${host}.crt -infiles ${host}.csr
     cat ${host}.key ${host}.crt > ${host}.pem   
 done 
 
@@ -132,12 +134,12 @@ done
 
 openssl genrsa  -out client1.key 2048
 openssl req -new -days 365 -key client1.key -out client1.csr -subj "$dn_prefix/OU=$ou_client/CN=client1"
-openssl ca -batch -notext -name SigningCA -config $root_ca_config -keyfile /home/vagrant/shared/certificate_sign/SigningCA/signing-ca.key -out client1.crt -infiles client1.csr
+openssl ca -batch -notext -name SigningCA -config $root_ca_config -extensions v3_req -keyfile $signing_ca_key -out client1.crt -infiles client1.csr
 cat client1.key client1.crt > client1.pem
 
 openssl genrsa  -out client2.key 2048
 openssl req -new -days 365 -key client2.key -out client2.csr -subj "$dn_prefix/OU=$ou_client/CN=client2"
-openssl ca -batch -notext -name SigningCA -config $root_ca_config -keyfile /home/vagrant/shared/certificate_sign/SigningCA/signing-ca.key -out client2.crt -infiles client2.csr
+openssl ca -batch -notext -name SigningCA -config $root_ca_config -extensions v3_req -keyfile $signing_ca_key -out client2.crt -infiles client2.csr
 cat client2.key client2.crt > client2.pem
 
 
